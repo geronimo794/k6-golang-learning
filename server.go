5375abf84453896c100b5b0fa52ab9fdd454dfec
Ach Rozikin <geronimo794@gmail.com>
2026-02-07 20:27:23 +0700

    fix: Init golang project

--- server.go
+++ server.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
+	loadEnv()
+	fmt.Println("Starting server on port 8080")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
