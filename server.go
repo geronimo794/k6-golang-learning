e2268e331c4ef257afc39253a10b7938e95aaf20
Ach Rozikin <geronimo794@gmail.com>
2026-02-07 20:36:37 +0700

    fix: Golang send

--- server.go
+++ server.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
+func connect() (*pgx.Conn, error) {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	conn, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
 func loadEnv() {
 	err := godotenv.Load()
 	if err != nil {
@@ -17,6 +28,11 @@ func loadEnv() {
 
 func main() {
 	loadEnv()
+	conn, err := connect()
+	if err != nil {
+		log.Fatal("Error connecting to database", err)
+	}
+	defer conn.Close(context.Background())
 	fmt.Println("Starting server on port 8080")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
